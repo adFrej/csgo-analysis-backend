c946a7c6a4c25512ba8fb70751d84f9facf88278
adFrej <adfrej1@gmail.com>
2023-01-25 19:04:36 +0100

    Added log level conf

--- csgoanalysis_app/apps.py
+++ csgoanalysis_app/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
+from django.conf import settings
 import os
 import logging
 # import warnings
 # warnings.filterwarnings("ignore")
 
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"),
+logging.basicConfig(level=os.environ.get("LOGLEVEL", settings.LOG_LEVEL),
                     format='%(asctime)s - %(name)s - [%(levelname)s]: %(message)s')
 logging.captureWarnings(True)
 
